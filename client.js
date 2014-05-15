6b702543c31c8ae69be564b6bca846b39260cc13
Rose Ames <rose@happyspork.com>
2014-05-15 17:37:19 -0400

    add sample conditional template rendering

--- client.js
+++ client.js
@@ -2,7 +2,12 @@ if (Meteor.isClient) {
   Template.letList.lets = function() {
     return Lets.find({});
   }
+
   Template.letList.count = function() {
     return Lets.find({}).count();
+  }
+
+  Template.letList.testFoo = function() {
+    return true;
   }
 }
