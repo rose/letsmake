7877b9f34acbeaf9bb1e445b980a98a0dbb5c94f
Rose Ames <rose@happyspork.com>
2014-05-27 10:15:44 -0400

    remove redundant sweetIdea() declaration

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-var randomElement = function(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-var sweetIdea = function() {
-  var adj1 = randomElement(["social", "cupcake", "reactive", "web 2.8", "function", "test"]);
-  var adj2 = randomElement(["networking", "oriented", "driven"]);
-  var thing = randomElement(["compiler", "app", "IDE", "framework", "solution"]);
-  var target = randomElement(["lemurs", "ostriches", "penguins", "doges", "aardvarks", "giraffes", "armadillos"]);
-  return ("a " + adj1 + " " + adj2 + " " + thing + " for " + target);
-}
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     Lets.remove({});
