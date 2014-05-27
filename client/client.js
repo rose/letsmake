db4238615bd7215478850202e0406dc11ca4e408
Rose Ames <rose@happyspork.com>
2014-05-27 17:00:11 -0400

    remove isClient test in client.js.  isServer seems necessary for some reason

--- client/client.js
+++ client/client.js
@@ -1,57 +1,55 @@
-if (Meteor.isClient) {
-  Session.set("show_new_lets_form", false);
+Session.set("show_new_lets_form", false);
 
-  Template.letList.lets = function() {
-    return Lets.find({});
+Template.letList.lets = function() {
+  return Lets.find({});
+}
+
+Template.letList.count = function() {
+  return Lets.find({}).count();
+}
+
+Template.letList.showingForm = function() {
+  return Session.get("show_new_lets_form");
+}
+
+Template.offerForm.events({
+  "click #getForm": function(event) {
+    Session.set("show_new_lets_form", true);
   }
+});
 
-  Template.letList.count = function() {
-    return Lets.find({}).count();
-  }
+Template.showForm.events({
+  "click #hideForm": function(event) {
+    Session.set("show_new_lets_form", false);
+  } 
+});
 
-  Template.letList.showingForm = function() {
-    return Session.get("show_new_lets_form");
-  }
-
-  Template.offerForm.events({
-    "click #getForm": function(event) {
-      Session.set("show_new_lets_form", true);
-    }
-  });
-
-  Template.showForm.events({
-    "click #hideForm": function(event) {
-      Session.set("show_new_lets_form", false);
-    } 
-  });
-
-  Template.showForm.events({
-    "keydown #title": function(event) {
-      if (event.which == 13) {
-        titleBox = $("#title");
-        if (titleBox.val().length > 4) {
-          Lets.insert({title: titleBox.val()});
-          titleBox.val("");
-        };
-      }
-
-      if (event.which == 27) {
-        Session.set("show_new_lets_form", false);
-      }
-    }
-  });
-
-  Template.showForm.events({
-    "click #addLets": function(event) {
+Template.showForm.events({
+  "keydown #title": function(event) {
+    if (event.which == 13) {
       titleBox = $("#title");
       if (titleBox.val().length > 4) {
         Lets.insert({title: titleBox.val()});
         titleBox.val("");
-      }
+      };
     }
-  });
 
-  Template.showForm.getSweetIdea = function() {
-    return sweetIdea();
-  };
-}
+    if (event.which == 27) {
+      Session.set("show_new_lets_form", false);
+    }
+  }
+});
+
+Template.showForm.events({
+  "click #addLets": function(event) {
+    titleBox = $("#title");
+    if (titleBox.val().length > 4) {
+      Lets.insert({title: titleBox.val()});
+      titleBox.val("");
+    }
+  }
+});
+
+Template.showForm.getSweetIdea = function() {
+  return sweetIdea();
+};
